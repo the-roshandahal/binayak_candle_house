fbbbc7565be13d59418f80b48f5aa74055089ad1
Roshan Dahal <roshandahal.r@gmail.com>
2023-08-18 17:42:58 +0545

    Update settings.py

--- binayak/settings.py
+++ binayak/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$38blkoui=7^o3j8usz)r)dy!gad^)zdj9e9pxgtxj=z#_bc8+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
