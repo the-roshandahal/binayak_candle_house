afd145362b8c92b7fe0f0c892df6206c374422d4
Roshan Dahal <roshandahal.r@gmail.com>
2023-08-18 17:31:32 +0545

    Update settings.py

--- binayak/settings.py
+++ binayak/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
