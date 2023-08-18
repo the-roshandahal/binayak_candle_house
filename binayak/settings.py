c93c03485510744ecc5a924e4c426864ab009762
Roshan Dahal <roshandahal.r@gmail.com>
2023-08-18 17:41:36 +0545

    Update settings.py

--- binayak/settings.py
+++ binayak/settings.py
@@ -120,15 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-STATIC_ROOT = 'binayakcandlehouse.com/staticfiles'
-MEDIA_ROOT = 'binayakcandlehouse.com/media'
+# MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "http://www.binayakcandlehouse.com/staticfiles/"
+MEDIA_URL = "http://www.binayakcandlehouse.com/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
